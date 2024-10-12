5b4eb2e897b8cb8c3980be947d0d02230d0386f5
tcyanhao <yanh8587@gmail.com>
2024-10-12 15:08:16 -0700

    Initial commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.138.0.2:9200"
+    ES_USERNAME = "yanhao"
+    ES_PASSWORD = "socialai"
+    GCS_BUCKET = "yan-202406"
+)
---
+++ service/user.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"fmt"
+	"reflect"
+
+	"socialai/backend"
+	"socialai/constants"
+	"socialai/model"
+
+	"github.com/olivere/elastic/v7"
+)
+
+func CheckUser(username, password string) (bool, error) {
+    query := elastic.NewBoolQuery()
+
+    query.Must(elastic.NewTermQuery("username", username))
+    query.Must(elastic.NewTermQuery("password", password))
+    searchResult, err := backend.ESBackend.ReadFromES(query, constants.USER_INDEX)
+    if err != nil {
+        return false, err
+    }
+
+    var utype model.User
+    for _, item := range searchResult.Each(reflect.TypeOf(utype)) {
+        u := item.(model.User)
+        if u.Password == password {
+            fmt.Printf("Login as %s\n", username)
+            return true, nil
+        }
+    }
+    return false, nil
+}
+
+
+func AddUser(user *model.User) (bool, error) {
+    // 1. check whether username already exsited
+    // 2. if not, add user to ES
+    query := elastic.NewTermQuery("username", user.Username)
+    searchResult, err := backend.ESBackend.ReadFromES(query, constants.USER_INDEX)
+    if err != nil {
+        return false, err
+    }
+
+    if searchResult.TotalHits() > 0 {
+        return false, nil
+    }
+
+    // save user to ES
+    err = backend.ESBackend.SaveToES(user, constants.USER_INDEX, user.Username)
+    if err != nil {
+        return false, err
+    }
+    fmt.Printf("User is added: %s\n", user.Username)
+    return true, nil
+}
